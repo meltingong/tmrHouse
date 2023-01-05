2887d7cb07caa81630b64774283ba48a94731f4e
seyeonkim <seyeonkim@localhost>
2023-01-05 19:15:02 +0900

    ..

--- src/com/itwill/tmr_house/member/erd/tmrHouse_update_select_delete.sql
+++ src/com/itwill/tmr_house/member/erd/tmrHouse_update_select_delete.sql
@@ -1,12 +1,22 @@
--- m_id, m_pw, m_name, m_birth, m_phone, m_address
-/**********************member update************************/
-update member set m_pw = 'abab', m_name '맹구', m_birth = '', m_pheon = '', m_address = '' where m_id = 'aaaa';
+/*
+이름        널?       유형            
+--------- -------- ------------- 
+M_ID      NOT NULL VARCHAR2(50)  
+M_PW               VARCHAR2(100) 
+M_NAME             VARCHAR2(100) 
+M_BIRTH            VARCHAR2(100) 
+M_PHONE            VARCHAR2(100) 
+M_ADDRESS          VARCHAR2(200) 
+ */
+
+/**********************member update pk************************/
+update member set m_pw = 'abab', m_name '맹구', m_birth = '990101', m_pheon = '1010-1010', m_address ='쌍떡잎마을' where m_id = 'aaaa';
 
 /**********************member delete************************/
-delete from member;
+delete from member where m_id = 'aaaa';
 
 /**********************member select by pk************************/
-select * from member where m_id = 'aaaa';
+select * from member where m_id = 'bbbb';
 
 /**********************member select all************************/
 select * from member;
