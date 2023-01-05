c3c4099c8d6a1687bdd8fd78f51cbec000783999
meltingong <hwk1210@naver.com>
2023-01-05 19:55:31 +0900

    ..

--- src/com/itwill/tmr_house/member/MemberDao.java
+++ src/com/itwill/tmr_house/member/MemberDao.java
@@ -30,6 +30,24 @@ private DataSource dataSource;
 		return rowCount;
 	}
 	
+	public int memberUpdate(Member m) throws Exception {
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_UPDATE);
+		pstmt.setString(1, m.getM_pw());
+		pstmt.setString(2, m.getM_name());
+		pstmt.setString(3, m.getM_birth());
+		pstmt.setString(4, m.getM_phone());
+		pstmt.setString(5, m.getM_address());
+		pstmt.setString(6, m.getM_id());
+		
+		int rowCount = pstmt.executeUpdate();
+		
+		pstmt.close();
+		dataSource.close(con);
+		
+		return rowCount;
+	}
+	
 	public int memberDelete(String m_id) throws Exception {
 		Connection con = dataSource.getConnection();
 		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_UPDATE);
@@ -38,6 +56,32 @@ private DataSource dataSource;
 		pstmt.close();
 		dataSource.close(con);
 		return rowCount;
+	}
+	
+	public Member findByID(String id) throws Exception {
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_SELECTED_BY_ID);
+		pstmt.setString(1, id);
+		
+		ResultSet rs = pstmt.executeQuery();
+		
+		Member findMember = null;
+		if (rs.next()) {
+			String i = rs.getString("m_id");
+			String pw = rs.getString("m_pw");
+			String name = rs.getString("m_name");
+			String birth = rs.getString("m_birth");
+			String phone = rs.getString("m_phone");
+			String address = rs.getString("m_address");
+			findMember = new Member(i, pw, name, birth, phone, address);
+		} else {
+			System.out.println("회원을 찾을 수 없습니다.");
+		}
+		
+		pstmt.close();
+		dataSource.close(con);
+		
+		return findMember;
 	}
 	
 	public List<Member> findAll() throws Exception{
--- src/com/itwill/tmr_house/member/test/MemberDaoTestMain.java
+++ src/com/itwill/tmr_house/member/test/MemberDaoTestMain.java
@@ -1,10 +1,16 @@
 package com.itwill.tmr_house.member.test;
 
+import com.itwill.tmr_house.member.Member;
+import com.itwill.tmr_house.member.MemberDao;
+
 public class MemberDaoTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		MemberDao memberDao = new MemberDao();
+		int rowCount = memberDao.memberDelete("aaaa");
+		System.out.println(">> " + rowCount + "행이 삭제되었습니다");
+		
+		rowCount = memberDao.memberInsert(new Member());
 	}
 
 }
