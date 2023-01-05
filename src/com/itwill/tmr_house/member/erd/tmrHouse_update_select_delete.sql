b1a80433e8a86cc97db1ed7981af044ecb288d31
seyeonkim <seyeonkim@localhost>
2023-01-05 19:25:28 +0900

    ..

--- src/com/itwill/tmr_house/member/erd/tmrHouse_update_select_delete.sql
+++ src/com/itwill/tmr_house/member/erd/tmrHouse_update_select_delete.sql
@@ -16,7 +16,7 @@ update member set m_pw = 'abab', m_name = '맹구', m_birth = '990101', m_pheon 
 delete from member where m_id = 'aaaa';
 
 /**********************member select by pk************************/
-select * from member where m_id = 'bbbb';
+select m_pw, m_name, m_birth, m_phone, m_address from member where m_id = 'bbbb';
 
 /**********************member select all************************/
-select * from member;
+select m_id, m_pw, m_name, m_birth, m_phone, m_address from member;
