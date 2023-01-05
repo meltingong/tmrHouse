460e0205ecafd2f4fc58ad03e64198d714e833ea
meltingong <hwk1210@naver.com>
2023-01-05 20:11:36 +0900

    ..

---
+++ src/com/itwill/tmr_house/member/김하은/MemberDaoService_H.java
@@ -0,0 +1,27 @@
+package com.itwill.tmr_house.member.김하은;
+
+import com.itwill.tmr_house.member.MemberDao;
+
+public class MemberDaoService_H {
+	private MemberDao memberDao;
+	
+	public MemberDaoService_H() throws Exception {
+		memberDao = new MemberDao();
+	}
+	/*
+	 * 회원가입 아이디가 중복되는 경우 메시지
+	 */
+	
+	/*
+	 * 아이디 존재여부 체크
+	 * 	- 아이디 존재하면 메시지
+	 *  - 아이디 존재하지 않으면 가입성공
+	 */
+	
+	/*
+	 * 회원 로그인
+	 */
+	
+	
+	
+}
