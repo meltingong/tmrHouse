acefa2460a518deec1a5bdbeebb3d341c3edf6f0
meltingong <hwk1210@naver.com>
2023-01-05 19:09:22 +0900

    ..

--- src/com/itwill/tmr_house/member/김하은/MemberDao_H.java
+++ src/com/itwill/tmr_house/member/김하은/MemberDao_H.java
@@ -1,5 +1,39 @@
 package com.itwill.tmr_house.member.김하은;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.itwill.tmr_house.common.DataSource;
+import com.itwill.tmr_house.member.Member;
+import com.itwill.tmr_house.member.MemberSQL;
+
 public class MemberDao_H {
+	private DataSource dataSource;
+	
+	
+	public MemberDao_H() throws Exception {
+		dataSource = new DataSource();
+	}
+	
+	public int memberInsert(Member member) throws Exception {
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(MemberSQL.MEMBER_INSERT);
+		pstmt.setString(1, member.getM_id());
+		pstmt.setString(2, member.getM_pw());
+		pstmt.setString(3, member.getM_name());
+		pstmt.setString(4, member.getM_birth());
+		pstmt.setString(5, member.getM_phone());
+		pstmt.setString(6, member.getM_address());
+		int rowCount = pstmt.executeUpdate();
+		pstmt.close();
+		dataSource.close(con);
+		return rowCount;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
