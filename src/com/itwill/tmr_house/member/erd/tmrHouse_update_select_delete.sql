4e4966d9071b32aeb82ced66567455d922cb216b
seyeonkim <seyeonkim@localhost>
2023-01-05 19:19:36 +0900

    ..

--- src/com/itwill/tmr_house/member/erd/tmrHouse_update_select_delete.sql
+++ src/com/itwill/tmr_house/member/erd/tmrHouse_update_select_delete.sql
@@ -9,7 +9,7 @@ M_PHONE            VARCHAR2(100)
 M_ADDRESS          VARCHAR2(200)
  */
 
-/**********************member update pk************************/
+/**********************member update************************/
 update member set m_pw = 'abab', m_name '맹구', m_birth = '990101', m_pheon = '1010-1010', m_address ='쌍떡잎마을' where m_id = 'aaaa';
 
 /**********************member delete************************/
