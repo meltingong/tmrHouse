3dfb4515b302826335073e202115a06b7a153513
meltingong <hwk1210@naver.com>
2023-01-05 18:51:29 +0900

    ..

--- src/com/itwill/tmr_house/member/MemberSQL.java
+++ src/com/itwill/tmr_house/member/MemberSQL.java
@@ -1,5 +1,11 @@
 package com.itwill.tmr_house.member;
 
 public class MemberSQL {
-
+	public static final String MEMBER_INSERT = "insert into member(m_id, m_pw, m_name, m_birth, m_phone, m_address) values(?,?,?,?,?,?)";
+	public static final String MEMBER_UPDATE = "";
+	public static final String MEMBER_DELETE = "";
+	public static final String MEMBER_SELECT = "";
+	public static final String MEMBER_SELECTED_BY_ID = "";
+	public static final String MEMBER_SELECTED_BY_ALL = "";
+	
 }
