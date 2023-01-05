aa34c9f2aefdb632f2ac703b6bbf3038c13e6fcc
meltingong <hwk1210@naver.com>
2023-01-05 19:58:17 +0900

    ..

--- src/com/itwill/tmr_house/member/test/MemberDaoTestMain.java
+++ src/com/itwill/tmr_house/member/test/MemberDaoTestMain.java
@@ -7,10 +7,19 @@ public class MemberDaoTestMain {
 
 	public static void main(String[] args) throws Exception {
 		MemberDao memberDao = new MemberDao();
+		System.out.println("회원 탈퇴");
 		int rowCount = memberDao.memberDelete("aaaa");
 		System.out.println(">> " + rowCount + "행이 삭제되었습니다");
 		
+		System.out.println("회원가입");
 		rowCount = memberDao.memberInsert(new Member());
+		System.out.println(">> " + rowCount + "행이 삽입되었습니다");
+		
+		System.out.println("회원수정");
+		
+		System.out.println("아이디로 회원정보 찾기");
+		
+		System.out.println("모든회원 리스트");
 	}
 
 }
