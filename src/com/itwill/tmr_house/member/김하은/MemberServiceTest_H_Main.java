4308b1928950a04759d3b64d6c1b8d7d56dc0386
meltingong <hwk1210@naver.com>
2023-01-06 17:05:53 +0900

    ..

--- src/com/itwill/tmr_house/member/김하은/MemberServiceTest_H_Main.java
+++ src/com/itwill/tmr_house/member/김하은/MemberServiceTest_H_Main.java
@@ -1,8 +1,31 @@
 package com.itwill.tmr_house.member.김하은;
 
+import com.itwill.tmr_house.member.Member;
+
 public class MemberServiceTest_H_Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		MemberService_H memberService = new MemberService_H();
+		/*
+		 * 회원가입
+		 */
+		System.out.println("회원가입");
+		memberService.addMember(new Member());
+		/*
+		 * 아이디 생성조건 있는 회원가입
+		 */
+		
+		/*
+		 * 회원정보수정
+		 */
+		
+		/*
+		 * 회원탈퇴
+		 */
+		
+		/*
+		 * 회원정보 상세보기
+		 */
 		
 	}
 
