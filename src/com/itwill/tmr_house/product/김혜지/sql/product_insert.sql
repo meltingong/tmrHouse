9759d1af41a12b98f0eedc8f1e3266cc121bb4b8
meltingong <hwk1210@naver.com>
2023-01-07 15:54:39 +0900

    ...

--- src/com/itwill/tmr_house/product/김혜지/sql/product_insert.sql
+++ src/com/itwill/tmr_house/product/김혜지/sql/product_insert.sql
@@ -13,9 +13,9 @@ P_FREEDELIVERY          VARCHAR2(50)
 
 /************************product insert************************/
 
-insert into product values (1, '스틸 원형 테이블', 120000, null, '미드센추리 모던 테이블', 'N');
-insert into product values (2, '원목 사각 테이블', 90000, null, '내추럴한 원목 테이블', 'N');
-insert into product values (3, '스틸 원목 의자', 110000, null, '미드센추리 모던 감성의 미니멀한 의자', 'Y');
-insert into product values (4, '라탄 원목 의자', 50000, null, '고무나무 원목과 라탄이 조화로운 의자', 'Y');
+insert into product values (product_p_no_SEQ.nextval, '스틸 원형 테이블', 120000, null, '미드센추리 모던 테이블', 'N');
+insert into product values (product_p_no_SEQ.nextval, '원목 사각 테이블', 90000, null, '내추럴한 원목 테이블', 'N');
+insert into product values (product_p_no_SEQ.nextval, '스틸 원목 의자', 110000, null, '미드센추리 모던 감성의 미니멀한 의자', 'Y');
+insert into product values (product_p_no_SEQ.nextval, '라탄 원목 의자', 50000, null, '고무나무 원목과 라탄이 조화로운 의자', 'Y');
 
 
