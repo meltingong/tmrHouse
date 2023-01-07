a51d104a1ee4fa938a56839ff3357f8ddbcb88b6
breeeeeeeeze <earth9563@gamil.com>
2023-01-07 23:37:14 +0900

    ..

--- src/com/itwill/tmr_house/product/ProductDao.java
+++ src/com/itwill/tmr_house/product/ProductDao.java
@@ -10,71 +10,84 @@ import com.itwill.tmr_house.common.DataSource;
 
 public class ProductDao {
 	private DataSource dataSource;
+
 	public ProductDao() throws Exception {
 		dataSource = new DataSource();
 	}
-	
-	/*
-	 * insert
-	 */
 
-	//상품 번호로 특정 상품 출력
-	public Product insert(Product product) throws Exception {
-		Product product = null;
-		Connection con=dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(ProductSQL.INSERT);
-		pstmt.setInt(1, p_no);
-		ResultSet rs=pstmt.executeQuery();
-		if(rs.next()) {
-			product = new Product(rs.getInt("p_no"),
-											rs.getString("p_name"),
-											rs.getInt("p_price"),
-											rs.getString("p_img"),
-											rs.getString("p_desc"),
-											rs.getString("p_freeDelivery"));
-		}
-		return product;
+	// insert(admin)
+	public int insert(Product product) throws Exception {
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_INSERT);
+		pstmt.setString(1, product.getP_name());
+		pstmt.setInt(2, product.getP_price());
+		pstmt.setString(3, product.getP_img());
+		pstmt.setString(4, product.getP_desc());
+		pstmt.setString(5, product.getP_freeDelivery());
+		int rowCount = pstmt.executeUpdate();
+		
+		pstmt.close();
+		dataSource.close(con);
+		return rowCount;
 	}
 
-	//상품 번호로 특정 상품 찾기
+	// delete by no(admin)
+	public int deleteByNo (int p_no) throws Exception {
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_DELETE_BY_P_NO);		
+		pstmt.setInt(1, p_no);
+		
+		int rowCount = pstmt.executeUpdate();
+		
+		pstmt.close();
+		dataSource.close(con);
+		return rowCount;
+	}
+	
+	// update(admin)
+	public int updateProduct(Product product) throws Exception {
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_UPDATE);
+		pstmt.setString(1, product.getP_name());
+		pstmt.setInt(2, product.getP_price());
+		pstmt.setString(3, product.getP_img());
+		pstmt.setString(4, product.getP_desc());
+		pstmt.setString(5, product.getP_freeDelivery());
+		pstmt.setInt(6, product.getP_no());
+		
+		int rowCount = pstmt.executeUpdate();
+		
+		pstmt.close();
+		dataSource.close(con);
+		return rowCount;
+	}
+
+	// 상품 번호로 특정 상품 찾기
 	public Product findByProductNo(int p_no) throws Exception {
 		Product product = null;
-		Connection con=dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_P_NO);
+		Connection con = dataSource.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_P_NO);
 		pstmt.setInt(1, p_no);
-		ResultSet rs=pstmt.executeQuery();
-		if(rs.next()) {
-			product = new Product(rs.getInt("p_no"),
-											rs.getString("p_name"),
-											rs.getInt("p_price"),
-											rs.getString("p_img"),
-											rs.getString("p_desc"),
-											rs.getString("p_freeDelivery"));
+		ResultSet rs = pstmt.executeQuery();
+		if (rs.next()) {
+			product = new Product(rs.getInt("p_no"), rs.getString("p_name"), rs.getInt("p_price"),
+					rs.getString("p_img"), rs.getString("p_desc"), rs.getString("p_freeDelivery"));
 		}
 		return product;
 	}
-	
-	
-	//상품 전체 찾기
+
+	// 상품 전체 찾기
 	public List<Product> findAll() throws Exception {
+		List<Product> productList = new ArrayList<Product>();
 		Connection con = dataSource.getConnection();
 		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_ALL);
 		ResultSet rs = pstmt.executeQuery();
-		
-		List<Product> productList = new ArrayList<Product>();
-		while(rs.next()) {
-			productList.add(new Product(rs.getInt("p_no"),
-										rs.getString("p_name"),
-										rs.getInt("p_price"),
-										rs.getString("p_img"),
-										rs.getString("p_desc"),
-										rs.getString("p_freeDelivery")));
+
+		while (rs.next()) {
+			productList.add(new Product(rs.getInt("p_no"), rs.getString("p_name"), rs.getInt("p_price"),
+					rs.getString("p_img"), rs.getString("p_desc"), rs.getString("p_freeDelivery")));
 		}
 		return productList;
 	}
 
-	
- 
-
 }
-
--- src/com/itwill/tmr_house/product/test/ProductDaoTestMain.java
+++ src/com/itwill/tmr_house/product/test/ProductDaoTestMain.java
@@ -1,9 +1,41 @@
 package com.itwill.tmr_house.product.test;
 
+import com.itwill.tmr_house.product.Product;
+import com.itwill.tmr_house.product.ProductDao;
+
 public class ProductDaoTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		ProductDao productDao = new ProductDao();
+
+		/*
+		 * 1~4 insert, update, delete
+		 */
+
+		// insert(admin)
+		System.out.println("1. Product_insert");
+		Product product = new Product(0, "장스탠드", 50000, "null", "반짝반짝", "N");
+		System.out.println(">> " + productDao.insert(product));
+		
+		/*
+		// delete(admin)
+		child record 있어서(orderitem이 fk로 product의 p_no를 가짐) 삭제할 수 없음
+		System.out.println(">> " + productDao.deleteByNo(4));
+		*/
+
+		// update(admin)
+		System.out.println("2. Product_update");
+		Product updateProduct = new Product(3, "edit", 11111, "edit_img", "edit_desc", "Y");
+		System.out.println(">> " + productDao.updateProduct(updateProduct));
+		System.out.println(productDao.findByProductNo(3));
+		
+		// 상품번호로 특정 상품 출력
+		System.out.println("3. Product_findByProductNo");
+		System.out.println(">> " + productDao.findByProductNo(1));
+
+		// 상품 전체 출력
+		System.out.println("4. Product_findAll");
+		System.out.println(">> " + productDao.findAll());
 
 	}
 
