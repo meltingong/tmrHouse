3ef9d4271f64aac4c9af31370577dec28bf40eaa
meltingong <hwk1210@naver.com>
2023-01-08 20:32:12 +0900

    ..

--- src/com/itwill/tmr_house/order/test/OrderServiceMain.java
+++ src/com/itwill/tmr_house/order/test/OrderServiceMain.java
@@ -7,13 +7,13 @@ public class OrderServiceMain {
 	public static void main(String[] args) throws Exception {
 		OrdersService ordersService = new OrdersService();
 		
-		// 주문 한개 삭제(주문번호)
+		/*// 주문 한개 삭제(주문번호)
 		int rowCount = ordersService.deleteByOrderNo(2);
 		System.out.println(">> " + rowCount + "건 주문 삭제");
 		
-		/*// 아이디로 주문 전체삭제
+		// 아이디로 주문 전체삭제
 		rowCount = ordersService.deleteByM_id("cccc");
-		System.out.println(">> cccc님 주문" + rowCount + "건 삭제" );
+		System.out.println(">> cccc님 주문" + rowCount + "건 삭제" );*/
 		
 		// 주문리스트 아이디로 찾기
 		System.out.println(ordersService.orderList("aaaa"));
@@ -22,11 +22,13 @@ public class OrderServiceMain {
 		System.out.println(ordersService.orderListDetail("aaaa",1));
 		
 		// 카트에서 주문
-		rowCount = ordersService.cartOrder("bbbb");
-		System.out.println(">> " + rowCount + "개 주문");*/
+		int rowCount = ordersService.cartOrder("bbbb");
+		System.out.println(">> " + rowCount + "개 주문");
 		
 		// 카트에서 선택주문
-		
+		String[] cart_item_checks = {"5"};
+		rowCount = ordersService.cartSelectOrder("bbbb",cart_item_checks);
+		System.out.println("bbbb님" + rowCount + "개 주문");
 		
 	}
 
