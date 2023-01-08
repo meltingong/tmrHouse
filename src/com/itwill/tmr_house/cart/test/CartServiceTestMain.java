780538b620314ebc06ceebe50478e93bf0932000
breeeeeeeeze <earth9563@gmail.com>
2023-01-09 08:53:54 +0900

    ..

--- src/com/itwill/tmr_house/cart/test/CartServiceTestMain.java
+++ src/com/itwill/tmr_house/cart/test/CartServiceTestMain.java
@@ -15,23 +15,23 @@ public class CartServiceTestMain {
 		System.out.println(">> " + cartService.insertCart(new Cart(1, 1, "cccc", productService.findByProductNo(1))));
 
 		// updateByCartNo
-		System.out.println("updateByCartNo");
+		System.out.println("2. updateByCartNo");
 		System.out.println(">> " + cartService.updateCartByCartNo(3, 10));
 
 		// updateByProductNo
-		System.out.println("updateByProductNo");
+		System.out.println("3. updateByProductNo");
 		System.out.println(">> " + cartService.updateCartByProductNo(3, 100, "bbbb"));
 
 		// 카트에 담긴 특정 상품 삭제
-		System.out.println("5. deleteCartItemByCartNo");
+		System.out.println("4. deleteCartItemByCartNo");
 		System.out.println(">> " + cartService.deleteCartItemByCartNo(4));
 
 		// 회원의 카트에 담긴 상품 전체 삭제
-		System.out.println("6. deleteCartItemByUserId");
+		System.out.println("5. deleteCartItemByUserId");
 		System.out.println(">> " + cartService.deleteCartItemByUserId("aaaa"));
 
 		// 카트에 담긴 특정 상품 찾기(카트 번호로)
-		System.out.println("7. findCartItemByCartNo");
+		System.out.println("6. findCartItemByCartNo");
 		System.out.println(">> " + cartService.findCartItemByCartNo(3));
 
 		// 회원의 카트에 담긴 상품 전체 찾기
