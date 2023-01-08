a0e40d5552b265a34d5770d657a7e981851eeb0e
민모 <@192.168.219.100>
2023-01-08 13:51:21 +0900

    ..

--- src/com/itwill/tmr_house/cart/성민모/CartSQL.java
+++ src/com/itwill/tmr_house/cart/성민모/CartSQL.java
@@ -2,7 +2,7 @@ package com.itwill.tmr_house.cart.성민모;
 
 public class CartSQL {
 	
-	public static String CART_UPDATE_BY_CART_NO;
-	public static String CART_COUNT_BY_PRODUCT_NO;
+	public static final String CART_UPDATE_BY_CART_NO = "update cart set cart_qty=? where cart_no=?";
+	public static String CART_COUNT_BY_PRODUCT_NO = "select count(*)  as p_count from cart c join userinfo u on c.m_id=u.m_id where u.m_id=? and c.p_no=?";
 
 }
