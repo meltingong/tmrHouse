56852bf8907bc09b383820dd7cb296362ffefb8b
kgee2 <kgee12300@gmail.com>
2023-01-08 15:01:56 +0900

    ..

--- src/com/itwill/tmr_house/cart/김혜지/CartSQL.java
+++ src/com/itwill/tmr_house/cart/김혜지/CartSQL.java
@@ -2,30 +2,33 @@ package com.itwill.tmr_house.cart.김혜지;
 
 public class CartSQL {
 	
-	
-	public static final String CART_INSERT = 
-			"insert into cart (c_no, c_qty, m_id, p_no) values (cart_cart_no_SEQ.nextval,1,'aaaa',1)";
-//	
-//	public static final String CART_COUNT_BY_USERID_PRODUCT_NO = 
-//			"select count(*)  as p_count from cart c join userinfo u on c.userid=u.userid where u.userid=? and c.p_no=?";
-//	//장바구니에 이미 추가된 상품을 추가할 때 (카트번호가 몇번인지 모르니까 상품번호, 회원번호로 검색)
-//	public static final String CART_UPDATE_BY_CART_NO = 
-//			"update cart set cart_qty=? where cart_no=?";
-//	
-//	public static final String CART_UPDATE_BY_PRODUCT_NO_USERID = 
-//			"update cart set cart_qty=cart_qty+? where userid=? and p_no=?";
-//		
-		public static final String CART_DELETE_BY_USERID =
-				"delete from cart where m_id = ?";
-		
-		public static final String CART_DELETE_BY_CART_NO =
-				"delete from cart where c_no = ?";
+	//카트에 새로운 상품 추가
+	public static final String CART_INSERT_CART = 
+			"insert into cart (c_no, c_qty, m_id, p_no) values (cart_cart_no_SEQ.nextval,?,?,?)";
 
-		public static final String CART_SELECT_BY_USERID =
-				"select c.c_no, c.c_qty, p.p_name, p.p_price, p.p_img from cart c join product p on c.p_no = p.p_no where m_id = ?";
+	/************성민모님 CartSQL에서 가져온 메서드*************/
+	public static final String CART_UPDATE_BY_CART_NO = 
+			"update cart set cart_qty=? where cart_no=?";
 
-		public static final String CART_SELECT_BY_CART_NO =
-				"select c.c_no, c.c_qty, p.p_name, p.p_price, p.p_img from cart c join product p on c.p_no = p.p_no where c_no = ?";
+	public static String CART_COUNT_BY_PRODUCT_NO = 
+			"select count(*)  as p_count from cart c join userinfo u on c.m_id=u.m_id where u.m_id=? and c.p_no=?";
+	/************************************************************/
+
+	//카트에 담긴 특정 상품 삭제
+	public static final String CART_DELETE_BY_CART_NO =
+			"delete from cart where c_no = ?";
+
+	//회원의 카트에 담긴 상품 전체 삭제
+	public static final String CART_DELETE_BY_USERID =
+			"delete from cart where m_id = ?";
+
+	//카트에 담긴 특정 상품 찾기(카트 번호로)
+	public static final String CART_SELECT_BY_CART_NO =
+			"select c.*,p.* from cart c join product p on c.p_no = p.p_no where c_no = ?";
+
+	//회원의 카트에 담긴 상품 전체 찾기
+	public static final String CART_SELECT_BY_USERID =
+			"select c.*,p.* from cart c join product p on c.p_no = p.p_no where m_id = ?";
 
 	}
 
