e895de93ef2b7806d0616dd665ef9023d413eff5
meltingong <hwk1210@naver.com>
2023-01-10 13:49:14 +0900

    Merge branch 'master' of https://github.com/2022-11-JAVA-DEVELOPER/java-project-team3-tmrhouse.git

--- src/com/itwill/tmr_house/order/ui/김하은/OrdersPanel_하은.java
+++ src/com/itwill/tmr_house/order/ui/김하은/OrdersPanel_하은.java
@@ -1,14 +1,76 @@
 package com.itwill.tmr_house.order.ui.김하은;
 
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
 
 public class OrdersPanel_하은 extends JPanel {
+	private JTable OrdersTable;
 
 	/**
 	 * Create the panel.
 	 */
 	public OrdersPanel_하은() {
+		setBackground(new Color(255, 255, 255));
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel OrdersNorthPanel = new JPanel();
+		OrdersNorthPanel.setBackground(new Color(255, 255, 255));
+		add(OrdersNorthPanel, BorderLayout.NORTH);
+		
+		JTextPane OrdersAppTitleTextPane = new JTextPane();
+		OrdersAppTitleTextPane.setText("Tomorrow's House");
+		OrdersAppTitleTextPane.setFont(new Font("D2Coding", Font.BOLD, 20));
+		OrdersNorthPanel.add(OrdersAppTitleTextPane);
+		
+		JPanel OrdersSouthPanel = new JPanel();
+		OrdersSouthPanel.setBackground(new Color(255, 255, 255));
+		add(OrdersSouthPanel, BorderLayout.SOUTH);
+		
+		JButton OrdersHomeButton = new JButton("");
+		OrdersSouthPanel.add(OrdersHomeButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		OrdersSouthPanel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		OrdersSouthPanel.add(btnNewButton_2);
+		
+		JPanel OrdersCenterPanel = new JPanel();
+		OrdersCenterPanel.setBackground(new Color(255, 255, 255));
+		add(OrdersCenterPanel, BorderLayout.CENTER);
+		OrdersCenterPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("주문내역");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("D2Coding", Font.BOLD, 30));
+		lblNewLabel.setBounds(107, 49, 269, 37);
+		OrdersCenterPanel.add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBackground(new Color(255, 255, 255));
+		scrollPane.setBounds(33, 210, 442, 43);
+		OrdersCenterPanel.add(scrollPane);
+		
+		OrdersTable = new JTable();
+		OrdersTable.setFont(new Font("D2Coding", Font.PLAIN, 12));
+		OrdersTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"\uC8FC\uBB38\uBC88\uD638", "\uC8FC\uBB38\uC0C1\uD488", "\uC8FC\uBB38\uB0B4\uC5ED", "\uC8FC\uBB38\uAE08\uC561", "\uC8FC\uBB38\uB0A0\uC9DC", "\uC8FC\uBB38\uC544\uC774\uB514"
+			}
+		));
+		scrollPane.setViewportView(OrdersTable);
 
 	}
-
 }
--- src/com/itwill/tmr_house/ui/TmrHouseMainFrame.java
+++ src/com/itwill/tmr_house/ui/TmrHouseMainFrame.java
@@ -7,7 +7,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class TmrHouseMainFrame extends JFrame {
-
+	
+	/*
+	 * 1. Service 객체선언
+	 */
+	
+	
 	private JPanel contentPane;
 
 	/**
